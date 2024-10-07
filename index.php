047f38915ae83c92771b5e9b615430b90fe9bef4
KrishnaDev <fjgj8842@gmail.com>
2024-10-07 08:34:38 +0700

    2

--- index.php
+++ index.php
@@ -4,23 +4,32 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Max-Age: 86400");
 header("Access-Control-Allow-Methods: GET");
 
-require ("lib/classline.php");
+require("lib/classline.php");
 
-if(isset($_GET["git"])){
-if($_GET["git"] == "gitsend"){
-$line = new Gitline();
-$response = $line->Gitsend($_GET["token"], $_GET["message"]);
-if($response["status"] == 200){
-  echo json_encode(array("สถานะ" => 200, "ข้อความ" => "ส่งข้อความสำเร็จแล้ว!"), JSON_UNESCAPED_UNICODE);
-}else{
-  echo json_encode(array("สถานะ" => $response["status"], "ข้อความ" => $response["message"]), JSON_UNESCAPED_UNICODE);
-  http_response_code(400);
+function kb_return($status, $message)
+{
+    if ($status === true) {
+        echo json_encode(array("สถานะ" => $status, "ข้อความ" => $message), JSON_UNESCAPED_UNICODE);
+        http_response_code(200);
+    } else if ($status === false) {
+        echo json_encode(array("สถานะ" => $status, "ข้อความ" => $message), JSON_UNESCAPED_UNICODE);
+        http_response_code(400);
+    }
 }
-}else{
-  echo json_encode(array("สถานะ" => 404,"ข้อความ"=> "ไม่พบจุดสิ้นสุดเอพีไอ!"), JSON_UNESCAPED_UNICODE);
-  http_response_code(400);
-}
-}else{
-  echo json_encode(array("สถานะ" => 404,"ข้อความ"=> "ไม่พบจุดสิ้นสุดเอพีไอ!"), JSON_UNESCAPED_UNICODE);
-  http_response_code(400);
+
+if (isset($_GET["git"])) {
+    if ($_GET["git"] == "gitsend") {
+        $line = new Gitline();
+        $response = $line->Gitsend($_GET["token"], $_GET["message"]);
+        
+        if ($response["status"] == 200) {
+            kb_return(true, "ส่งข้อความสำเร็จแล้ว!");
+        } else {
+            kb_return(false, "เกิดข้อผิดพลาดในการส่งข้อความ!");
+        }
+    } else {
+        kb_return(false, "ไม่พบจุดสิ้นสุดเอพีไอ!");
+    }
+} else {
+    kb_return(false, "ไม่พบจุดสิ้นสุดเอพีไอ!");
 }
